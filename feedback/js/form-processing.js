cc904ee01f4c8877e9e2bd3ff3354a21fd40bbca
Евгений Пономарев <ponomareve45@gmail.com>
2023-02-18 18:43:01 +0300

    developed a form for sending a letter

--- feedback/js/form-processing.js
+++ feedback/js/form-processing.js
@@ -1,65 +1,9 @@
-$(document).ready(function() { 
-    var button = $('#button-up');	
-    $(window).scroll (function () {
-      if ($(this).scrollTop () > 300) {
-        button.fadeIn();
-      } else {
-        button.fadeOut();
-      }
-  });	 
-  button.on('click', function(){
-  $('body, html').animate({
-  scrollTop: 0
-  }, 800);
-  return false;
-  });		 
-  });
-  $(function() {
-    // Owl Carousel
-    var owl = $("#js-carousel-1");
-    owl.owlCarousel({
-      items: 1,
-      margin: 0,
-      loop: true,
-        items: 1,
-        dots: true,
-        pagination : true,
-        responsive: {
-          500: {
-            // Количество элементов 2
-            items: 1,
-            // Включаем точки
-            dots: true
-          },
-      
-          // Ширина от 800 пикселей
-          800: {
-            // Количество элементов 3
-            items: 1,
-            // Включаем точки
-            dots: true
-          }
-        }
-    });
-  });
-
-
-  $(document).ready(function() {
-    $("#your_link").fancybox({
-      hideOnOverlayClick:true,
-      hideOnContentClick:true
-    });
-  });
-  $(document).ready(function() {
-    $(".menuToggle").click(function() {
-      $(this).toggleClass("active");
-      $('.menu').slideToggle(300, function(){
-        if($(this).css('display') === "none"){
-          $(this).removeAttr('style');
-        }
-      });
-    });
-  });
+/*!
+ * Форма обратной связи (https://github.com/itchief/feedback-form)
+ * Описание: https://itchief.ru/php/feedback-form
+ * Copyright 2016-2022 Alexander Maltsev
+ * Licensed under MIT (https://github.com/itchief/feedback-form/blob/master/LICENSE)
+ */
 
 class ItcSubmitForm {
 
